378d5e3df032604445a315bed04a7fd5e79ce1bf
basamaryan <basam.aryan@gmail.com>
2025-12-13 18:02:38 -0800

    sm6375-common: overlay: Reduce blur radius
    Change-Id: Id6339df82156099ca14da684f236931b3ecf51ce

--- sm6375.mk
+++ sm6375.mk
@@ -213,6 +213,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
+    Launcher3DeviceOverlay \
     NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
